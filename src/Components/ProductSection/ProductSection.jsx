f45af2489aac3b5578f64ca0703bc273fa847d96
A-MANSY <ahmedmelmansy26@gmail.com>
2026-02-24 10:59:11 +0200

    Fix imports and build issues, update production build

--- src/Components/ProductSection/ProductSection.jsx
+++ src/Components/ProductSection/ProductSection.jsx
@@ -3,8 +3,8 @@
   import styles from './ProductSection.module.css'
   import { useCategroyContext } from '../../Contexts/CategroyContext/CategroyProvider';
   import { getProductsByCategory } from '../../Api/ApiProduct';
-  import Details from '../SectionsComponents/details/details';
-  import ProductSlide from '../ProductStyle/ProductSlide';
+  // import Details from '../SectionsComponents/details/details';
+    import ProductSlide from '../ProductStyle/ProductSlide';
   import SliderComponent from '../SliderComponent/SliderComponent';
   export default function ProductSection({title, category, description }) {
       const{BASE_URL,} = useCategroyContext()
@@ -23,10 +23,8 @@
     }, [BASE_URL]);
     return (
 <div className= {styles.smart_phone}>
-        <Details>
         <h1 className="details">{title}</h1>
         <p>{description}</p>
-        </Details>
 <SliderComponent>
   {data.map(product => (
     <SwiperSlide key={product.id}>
